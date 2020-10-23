6c7af07656b7d475722f6eb471d89ef9ad37b3db
Sagor <mbrsagor@gmail.com>
2020-10-23 14:22:16 +0600

    User login logout done

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from base.views.dashboard import Dashboard
+from base.views.dashboard_view import Dashboard
+from base.views.auth_view import UserLoginView, LogoutView
 
 urlpatterns = [
-    path('', Dashboard.as_view(), name='dashboard'),
+    path('', UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
 ]
