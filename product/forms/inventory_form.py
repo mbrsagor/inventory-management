4803febc10b4e23c87c92c67389d85f21112450d
Sagor <mbrsagor@gmail.com>
2020-10-24 00:24:09 +0600

    Inventory created page beautification

--- product/forms/inventory_form.py
+++ product/forms/inventory_form.py
@@ -9,10 +9,13 @@ class InventoryForm(forms.ModelForm):
         model = Inventory
         fields = '__all__'
         widgets = {
-            'name': TextInput(attrs={'class': 'form-control', 'id': 'name'}),
+            'name': TextInput(attrs={'class': 'form-control', 'id': 'name', 'placeholder': 'Enter Inventory Name'}),
             'category_name': Select(attrs={'class': 'form-control', 'id': 'category_name'}),
-            'short_description': TextInput(attrs={'class': 'form-control', 'id': 'short_description'}),
-            'full_description': Textarea(attrs={'class': 'form-control', 'id': 'full_description'}),
+            'short_description': TextInput(
+                attrs={'class': 'form-control', 'id': 'short_description',
+                       'placeholder': 'Enter inventory shot description'}),
+            'full_description': Textarea(attrs={'class': 'form-control', 'id': 'full_description',
+                                                'placeholder': 'Enter inventory full description'}),
             'current_stock': NumberInput(attrs={'class': 'form-control', 'id': 'current_stock'}),
             'purchase_price': NumberInput(attrs={'class': 'form-control', 'id': 'purchase_price'}),
             'sales_price': NumberInput(attrs={'class': 'form-control', 'id': 'sales_price'}),
