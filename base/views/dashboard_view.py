31c5741aebb8b67104f5ebf8c5e4deae372c77f0
Sagor <mbrsagor@gmail.com>
2020-10-26 12:20:49 +0600

    Dashboard cateogry and product counter show

--- base/views/dashboard_view.py
+++ base/views/dashboard_view.py
@@ -2,6 +2,9 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+from product.models.category import Category
+from product.models.inventory import Inventory
+
 
 class Dashboard(TemplateView):
     template_name = 'dashboard/dashboard.html'
@@ -9,3 +12,9 @@ class Dashboard(TemplateView):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(Dashboard, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(Dashboard, self).get_context_data(**kwargs)
+        context['category'] = Category.objects.all()
+        context['inventory'] = Inventory.objects.all()
+        return context
