6394fc87d4e832b7ea88db198d7947f3ec7c27c5
Sagor <mbrsagor@gmail.com>
2020-10-27 02:26:11 +0600

    Paginate implement and UI modal beautification in tag

--- base/views/tag_view.py
+++ base/views/tag_view.py
@@ -2,7 +2,7 @@ from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.messages.views import SuccessMessageMixin
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from base.models.tag import Tag
 from base.forms.tag_form import TagForm
 
@@ -15,3 +15,19 @@ class CreateListTagView(SuccessMessageMixin, CreateView, ListView):
     success_message = "Category successfully created!"
     success_url = '/tag/'
     context_object_name = 'tag'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateListTagView, self).get_context_data(**kwargs)
+        tag = self.get_queryset()
+        page = self.request.GET.get('page')
+        paginator = Paginator(tag, self.paginate_by)
+
+        try:
+            tag = paginator.page(page)
+        except PageNotAnInteger:
+            tag = paginator.page(1)
+        except EmptyPage:
+            tag = paginator.page(paginator.num_pages)
+        context['tag'] = tag
+        return context
